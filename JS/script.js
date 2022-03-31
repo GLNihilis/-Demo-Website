e5860c3de986c640dccb7e6c036482fb4bcbede5
GLNihilis <Nihilis9999@gmail.com>
2022-03-31 22:34:05 +0700

    product - review - blog

--- JS/script.js
+++ JS/script.js
@@ -49,8 +49,9 @@
                 }
     },
 
-    ]
+  ]
     });
+
 
     //game part js
     $('.match-main').slick({
@@ -120,73 +121,140 @@
   ]
     });
 
-  //player part js
-  $('.player-main').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    dots: false,
-    arrows: false,
-    autoplaySpeed: 2500,
-    centerMode: true,
-    centerPadding: '0px',
-    focusOnSelect: true,
-    responsive: [
-        {
-            breakpoint: 576,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-},
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
-},
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
-},
+    //player part js
+    $('.player-main').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        autoplaySpeed: 2500,
+        centerMode: true,
+        centerPadding: '0px',
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+    },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+    },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+    },
 
-]
-});
+  ]
+    });
 
-//product part js
-$('.product-main').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    dots: false,
-    arrows: false,
-    autoplaySpeed: 2500,
-    responsive: [
-        {
-            breakpoint: 576,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-},
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
-},
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
-},
+    //product part js
+    $('.product-main').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        autoplaySpeed: 2500,
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+    },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+    },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+    },
 
-]
-});
+  ]
+    });
+
+    // review slide js part
+    $('.review-img-main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        autoplaySpeed: 2500,
+        infinite: true,
+        fade: true,
+        asNavFor: '.review-txt-main',
+    });
+
+    $('.review-txt-main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        autoplaySpeed: 2500,
+        fades: true,
+        infinite: true,
+        speed: 1500,
+        fade: true,
+        asNavFor: '.review-img-main',
+    });
+
+
+    //blog part js
+    $('.blog-main').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: true,
+        nextArrow: '.blog-arrow .arrow-ar',
+        prevArrow: '.blog-arrow .arrow-al',
+        autoplaySpeed: 2500,
+        centerMode: true,
+        centerPadding: '0px',
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+    },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+    },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+    },
+
+  ]
+    });
