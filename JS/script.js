a0f8455a776adfd03073b0536df950a35dc215ae
NguyenVanHieu <2051063810@e.tlu.edu.vn>
2022-03-31 22:25:14 +0700

    game-brand part

--- JS/script.js
+++ JS/script.js
@@ -51,3 +51,71 @@
 
     ]
     });
+
+    //game part js
+    $('.match-main').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        autoplaySpeed: 2500,
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+    },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+    },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+    },
+
+  ]
+    });
+
+    //brand part js
+    $('.brand-main').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        autoplaySpeed: 2500,
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+    },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+    },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+    },
+
+  ]
+    });
