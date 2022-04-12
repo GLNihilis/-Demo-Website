73fb597df64433a5a6446bf90e9fd66384244241
NguyenVanHieu <2051063810@e.tlu.edu.vn>
2022-04-13 00:29:45 +0700

    admin sua

--- JS/adminscript.js
+++ JS/adminscript.js
@@ -1,16 +1,15 @@
 const btn_toggle = document.querySelector('.btn-toggle');
 const sidebar = document.querySelector('.sidebar');
 const logo = document.querySelector('.logo');
+const wapper = document.querySelector('.wapper')
 
 btn_toggle.addEventListener("click", () => {
     sidebar.classList.toggle("active");
     if(sidebar.classList.contains('active')) {
         logo.setAttribute("style", "display: flex");
-        wrapper.setAttribute("style", "left: 240px; width: calc (100% - 240px)")
-        table.setAttribute("style", "left: 240px; width: calc (100% - 240px)")
+        wapper.setAttribute("style", "left: 240px; width: calc (100% - 240px)")
         return
     }
     logo.setAttribute("style", "display: none")
-    wrapper.setAttribute("style", "left: 85px")
-    table.setAttribute("style", "display: none")
+    wapper.setAttribute("style", "left: 85px")
 })
